969a4c4a8dc281aba98fcc594681521c8181a0d4
Briankipr <Brayos718@gmail.com>
2023-09-19 17:56:34 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,42 +8,36 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string:param>')
-def print_string(param):
-    # Print the string in the console
-    print(param)
-    # Display the string in the web browser
-    return f'<p>Printed string: {param}</p>'
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
 
-@app.route('/count/<int:param>')
-def count(param):
-    # Create a string containing numbers in the range of the parameter
-    numbers = '\n'.join(str(i) for i in range(param + 1))
-    # Display the numbers in the web browser
-    return f'<pre>{numbers}</pre>'
+@app.route('/count/<int:number>')
+def count(number):
+    count = ''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
-@app.route('/math/<float:num1><operation><float:num2>')
-def math(num1, operation, num2):
-    result = None
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
     if operation == '+':
-        result = num1 + num2
+        return str(num1 + num2)
+    
     elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            return "Division by zero is not allowed."
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return "Invalid operation. Supported operations: +, -, *, div, %"
+        return str(num1 - num2)
 
-    return f'<p>{num1} {operation} {num2} = {result}</p>'
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(port=5555, debug=True)
